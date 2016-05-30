0b1578e3cf8742344fac382362a80146abbfeee8
flute19 <alexia.lequilliec@orange.fr>
2016-05-30 11:30:18 +0200

    Add files via upload

--- addToProj.php
+++ addToProj.php
@@ -9,7 +9,7 @@
 	File name : add.php
 	Aim : Add data in table specified in "connect.php" file. Check that data are complete. 
 	
-	last update : 29/05 par Alexia
+	last update : 04/05 par Clement
 	
 -->
 	</HEAD>
@@ -24,46 +24,73 @@ include "connect.php";
 <?php
 
 $vProjet = $_POST["projet"];
-$vEmploye1 = $_POST["employé1"];
-$pattern ='/[a-zA-Z]+/';
+$vEmploye1Nom = $_POST["employe1name"];
+$vEmploye1Prenom = $_POST["employe1firstname"];
+
+
+//char autorize for data given by user
+$pattern1 = '/[a-zA-Z0-9]+/'; //project name
+$pattern2 ='/[a-zA-Z]+/'; //employe's name and surname
+
+$vOk = TRUE; //test if insertion ok.
+
 
 /*test if data are under correct form via reg exp. 
  if not, refuse insertion. 
- else, ask for connection to database and try inserting data. */
+ else, ask for connection to d$vQuery1atabase and try inserting data. */
  
-if(preg_match($pattern, $vProjet, $matches)){
+ if (!preg_match($pattern1, $vProjet, $matches)) {
+	 $vOk = False;
+	 echo "$vProjet n'est pas une donnée acceptable.";
+	}
+if (!preg_match($pattern2, $vEmploye1Nom, $matches)) {
+	 $vOk = False;
+	 echo "$vEmploye1Nom n'est pas une donnée acceptable.";
+	}
+if (!preg_match($pattern2, $vEmploye1Prenom, $matches)) {
+	 $vOk = False;
+	 echo "$vEmploye1Prenom n'est pas une donnée acceptable.";
+	}
+
+if($vOk){//field have correct data form
 	
 	$vConn = fConnect(); //connection to database
-
-	if ($vConn) {//if connected
-		$vSql1 = "SELECT pk_num_badge FROM Employe AS E WHERE E.nom='$vEmploye1'";
-		$vQuery1 = pg_query($vConn, $vSql1);
-		
-		//user feedback.
-		if($vQuery1) 
-			echo "Insertion done";
-		 else 
-			 echo "Error. Insertion fail";
-		 
-		 //close connection
-		 pg_close($vConn);
-		 
-	} else { //no connection to database made
-		echo "Une erreur a eu lieu, la connection a échoué.";
-		}
-	}else {
-		echo " $v. Erreur, données erronées.";
-}
 	
 	if ($vConn) {//if connected
-		$vSql1 = "INSERT INTO Participants_Aux_Projets (pfk_employe, pfk_projet ) VALUES ('$vEmploye1','$vProjet')";
+	
+		//get proper attributes to be added to db.
+		// acces via info provided by user. 
+		//1: projet, 2: employe1
+		$vSql1 = "SELECT P.pk_key FROM Projet AS P WHERE P.nom='$vProjet'";
 		$vQuery1 = pg_query($vConn, $vSql1);
+		$vResult1 = pg_fetch_array($vQuery1); // get primary key for projet
+		
+		if(!$vResult1){
+			$vOk=FALSE;//prevent insertion of null in db
+			echo "Le projet $vProjet n'existe pas dans la base de donnée. Veuillez l'ajouter. ";
+			}
+		
+		$vSql2 = "SELECT pk_num_badge FROM Employe AS E WHERE E.nom='$vEmploye1Nom' AND E.prenom='$vEmploye1Prenom'";
+		$vQuery2 = pg_query($vConn, $vSql2);
+		$vResult2 = pg_fetch_array($vQuery2); // get primary key for employe
+		
+		
+		if(!$vResult2){
+			$vOk=FALSE; //prevent insertion in db
+			echo "L'employé $vEmploye1Nom $vEmploye1Prenom n'existe pas dans la base de donnée. Veuillez l'ajouter. ";
+			}
+		
+		//insertion in db
+		if ($vOk){//check if possible
+			$vSql3 = "INSERT INTO Participants_Aux_Projets (pfk_employe, pfk_projet ) VALUES ($vResult2[pk_num_badge], $vResult1[pk_key])";
+			$vQuery3 = pg_query($vConn, $vSql3);
+		}
 		
 		//user feedback.
-		if($vQuery1) 
+		if($vQuery3) 
 			echo "Insertion done";
 		 else 
-			 echo "Error. Insertion fail";
+			 echo "Error. Insertion failed.";
 		 
 		 //close connection
 		 pg_close($vConn);
@@ -71,8 +98,8 @@ if(preg_match($pattern, $vProjet, $matches)){
 	} else { //no connection to database made
 		echo "Une erreur a eu lieu, la connection a échoué.";
 		}
-	}else {
-		echo " $v. Erreur, données erronées.";
+}else {
+	echo " Erreur, données erronées.";
 }
 
 ?> 
--- formAddToProj.php
+++ formAddToProj.php
@@ -6,14 +6,30 @@
 <BODY>
  <h1> Ajout d'un acteur à un projet</h1>
 
+ <h2>  Saisie manuelle  </h2>
+ 
  <FORM METHOD='POST' ACTION="addToProj.php"> 
-	Projet concerné:
-	<INPUT NAME="projet">
-	Employé concerné: 
-	<INPUT NAME="employe1"> <!-- ultimately, we want to allowed people to add several people at the same tame-->
+ <table>
+ <tr>
+	<td> Projet concerné: </td>
+	<td><INPUT NAME="projet"></td>
+</tr>
+<tr>
+	<td> Employé concerné: </td> 
+	<td>Nom :<INPUT NAME="employe1name"></td> <!-- ultimately, we want to allowed people to add several people at the same tame-->
+	<td>Prénom: <INPUT NAME="employe1firstname"></td>
+</tr>
+<tr>
 	<INPUT VALUE="AJOUTER" TYPE="SUBMIT">
+</tr>
+</table>
 
 </FORM>
+
+
+
+ 
+ 
   <a href="accueil.html">Add site</a>
 
 
